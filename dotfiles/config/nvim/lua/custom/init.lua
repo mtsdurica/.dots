d30d1fedbdcc03f0dee8d5fdabcd01fe744acdc6
Matus Durica <mtsdurica@gmail.com>
2023-10-23 18:43:39 +0200

    Add custom nvchad configs

---
+++ dotfiles/config/nvim/lua/custom/init.lua
@@ -0,0 +1,10 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
