32effc56fbc5a88fb08d878832ab4bca408886d9
Matus Durica <mtsdurica@gmail.com>
2023-10-23 23:26:14 +0200

    Update

--- dotfiles/config/nvim/lua/custom/chadrc.lua
+++ dotfiles/config/nvim/lua/custom/chadrc.lua
@@ -5,11 +5,14 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "onedark",
-  theme_toggle = { "onedark", "one_light" },
+  theme = "gruvchad",
+  theme_toggle = { "gruvchad", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+  statusline = {
+    theme = "vscode_colored"
+  },
   nvdash = {
     load_on_startup = true,
   },
--- dotfiles/config/nvim/lua/custom/configs/null-ls.lua
+++ dotfiles/config/nvim/lua/custom/configs/null-ls.lua
@@ -1,25 +1,28 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 
 local b = null_ls.builtins
 
 local sources = {
 
-  -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+	-- webdev stuff
+	b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+	b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
 
-  -- Lua
-  b.formatting.stylua,
+	-- Lua
+	b.formatting.stylua,
 
-  -- cpp
-  b.formatting.clang_format.with({
-    extra_args = {
-      "-style=Microsoft",
-    },
-  }),
+    -- PHP
+	b.formatting.phpcsfixer,
+
+	-- cpp
+	b.formatting.clang_format.with({
+		extra_args = {
+			"-style=Microsoft",
+		},
+	}),
 }
 
-null_ls.setup {
-  debug = true,
-  sources = sources,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+})
